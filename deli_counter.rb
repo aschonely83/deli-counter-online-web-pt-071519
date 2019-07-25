28479e98b2187f4122cdadaa3b37e4a11cadeada
Adam Schonely <aschonely@gmail.com>
2019-07-25 00:43:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(deli)
     return "The line is currently empty."
   else 
     current_line = " The current line is:"
-    deli.each_with_index
+    deli.each_with_index(1) do |people, |
     
 
 end   
